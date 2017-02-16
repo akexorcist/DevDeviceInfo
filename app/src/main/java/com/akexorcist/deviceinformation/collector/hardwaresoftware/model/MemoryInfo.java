f80e8e105c70af6b7a122aa6fe177dc7ac596853
Akexorcist <akexorcist@gmail.com>
2017-02-17 01:13:43 +0700

    Migrate MemoryInfo to BaseInfo

--- app/src/main/java/com/akexorcist/deviceinformation/collector/hardwaresoftware/model/MemoryInfo.java
+++ app/src/main/java/com/akexorcist/deviceinformation/collector/hardwaresoftware/model/MemoryInfo.java
@@ -1,58 +1,54 @@
 package com.akexorcist.deviceinformation.collector.hardwaresoftware.model;
 
 import com.akexorcist.deviceinformation.common.BaseInfo;
-import com.google.gson.annotations.SerializedName;
 
 /**
  * Created by Akexorcist on 11/22/2016 AD.
  */
 
 public class MemoryInfo extends BaseInfo {
-    @SerializedName("Total Memory")
-    private String totalMemory;
-    @SerializedName("Heap Size")
-    private String heapSize;
-    @SerializedName("Heap Start Size")
-    private String heapStartSize;
-    @SerializedName("Heap Growth Limit")
-    private String heapGrowthLimit;
+    private static final String TOTAL_MEMORY = "Total Memory";
+    private static final String HEAP_SIZE = "Heap Size";
+    private static final String HEAP_START_SIZE = "Heap Start Size";
+    private static final String HEAP_GROWTH_LIMIT = "Heap Growth Limit";
 
     public MemoryInfo() {
+        super();
     }
 
     public String getTotalMemory() {
-        return totalMemory;
+        return getValueByTitle(TOTAL_MEMORY);
     }
 
     public MemoryInfo setTotalMemory(String totalMemory) {
-        this.totalMemory = totalMemory;
+        setDataInfo(TOTAL_MEMORY, totalMemory);
         return this;
     }
 
     public String getHeapSize() {
-        return heapSize;
+        return getValueByTitle(HEAP_SIZE);
     }
 
     public MemoryInfo setHeapSize(String heapSize) {
-        this.heapSize = heapSize;
+        setDataInfo(HEAP_SIZE, heapSize);
         return this;
     }
 
     public String getHeapStartSize() {
-        return heapStartSize;
+        return getValueByTitle(HEAP_START_SIZE);
     }
 
     public MemoryInfo setHeapStartSize(String heapStartSize) {
-        this.heapStartSize = heapStartSize;
+        setDataInfo(HEAP_START_SIZE, heapStartSize);
         return this;
     }
 
     public String getHeapGrowthLimit() {
-        return heapGrowthLimit;
+        return getValueByTitle(HEAP_GROWTH_LIMIT);
     }
 
     public MemoryInfo setHeapGrowthLimit(String heapGrowthLimit) {
-        this.heapGrowthLimit = heapGrowthLimit;
+        setDataInfo(HEAP_GROWTH_LIMIT, heapGrowthLimit);
         return this;
     }
 }
