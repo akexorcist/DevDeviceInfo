22e8d397277fd2e95477ef00f4c2c2be6772ae68
Akexorcist <akexorcist@gmail.com>
2017-02-18 01:00:48 +0700

    Collect sensor info then show on sensor info recycler view

--- app/src/main/java/com/akexorcist/deviceinformation/module/sensor/SensorFragment.java
+++ app/src/main/java/com/akexorcist/deviceinformation/module/sensor/SensorFragment.java
@@ -1,16 +1,40 @@
 package com.akexorcist.deviceinformation.module.sensor;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.FrameLayout;
 
 import com.akexorcist.deviceinformation.R;
-import com.akexorcist.deviceinformation.common.BaseDdiFragment;
+import com.akexorcist.deviceinformation.collector.sensor.SensorInfoCollector;
+import com.akexorcist.deviceinformation.collector.sensor.model.SensorInfo;
+import com.akexorcist.deviceinformation.collector.sensor.model.SensorItem;
+import com.akexorcist.deviceinformation.common.DdiFragment;
+import com.akexorcist.deviceinformation.utility.RxGenerator;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action0;
+import rx.functions.Action1;
+import rx.functions.Func1;
+import rx.schedulers.Schedulers;
 
 /**
  * Created by Akexorcist on 11/20/2016 AD.
  */
 
-public class SensorFragment extends BaseDdiFragment {
+public class SensorFragment extends DdiFragment {
+    private FrameLayout layoutSensorLoading;
+    private FrameLayout layoutSensorContent;
+    private SwipeRefreshLayout srlRefresh;
+    private RecyclerView rvContent;
+    private SensorContentAdapter contentAdapter;
 
     public static SensorFragment newInstance() {
         return new SensorFragment();
@@ -26,12 +50,21 @@ public class SensorFragment extends BaseDdiFragment {
 
     @Override
     protected void bindView(View view) {
-
+        layoutSensorContent = (FrameLayout) view.findViewById(R.id.layout_sensor_content);
+        layoutSensorLoading = (FrameLayout) view.findViewById(R.id.layout_sensor_loading);
+        srlRefresh = (SwipeRefreshLayout) view.findViewById(R.id.srl_sensor_refresh);
+        rvContent = (RecyclerView) view.findViewById(R.id.rv_sensor_content);
     }
 
     @Override
     protected void setupView() {
-
+        setContentLayout(layoutSensorContent);
+        setLoadingLayout(layoutSensorLoading);
+        srlRefresh.setOnRefreshListener(onContentRefresh());
+        rvContent.setNestedScrollingEnabled(false);
+        rvContent.setLayoutManager(new LinearLayoutManager(getContext()));
+        contentAdapter = new SensorContentAdapter();
+        rvContent.setAdapter(contentAdapter);
     }
 
     @Override
@@ -41,7 +74,8 @@ public class SensorFragment extends BaseDdiFragment {
 
     @Override
     protected void initialize() {
-
+        forceHideContent();
+        collectSensorInfo();
     }
 
     @Override
@@ -57,5 +91,43 @@ public class SensorFragment extends BaseDdiFragment {
     @Override
     public void saveInstanceState(Bundle outState) {
 
+    }
+
+    private SwipeRefreshLayout.OnRefreshListener onContentRefresh() {
+        return () -> {
+            hideContent();
+            srlRefresh.setRefreshing(false);
+            RxGenerator.getInstance().createDelayObservable(1, TimeUnit.SECONDS)
+                    .doOnCompleted(refreshAllInfoAction())
+                    .subscribe();
+        };
+    }
+
+    private Action0 refreshAllInfoAction() {
+        return this::collectSensorInfo;
+    }
+
+    private void collectSensorInfo() {
+        createCollectSensorInfoObservable(getContext())
+                .flatMap(mapSensorInfoToSensorItem())
+                .subscribe(showSensorInfo());
+    }
+
+    private Func1<SensorInfo, Observable<List<SensorItem>>> mapSensorInfoToSensorItem() {
+        return sensorInfo -> Observable.just(sensorInfo.getSensorItemList());
+    }
+
+    private Observable<SensorInfo> createCollectSensorInfoObservable(Context context) {
+        return Observable.just(SensorInfoCollector.getInstance().collect(context))
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+    }
+
+    private Action1<List<SensorItem>> showSensorInfo() {
+        return sensorItemList -> {
+            contentAdapter.setSensorItemList(sensorItemList);
+            contentAdapter.notifyDataSetChanged();
+            showContent();
+        };
     }
 }
