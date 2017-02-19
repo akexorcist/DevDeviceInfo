a373477d7518683f98cb33221060261f4558955c
Akexorcist <akexorcist@gmail.com>
2017-02-19 10:01:05 +0700

    Create unknown view holder

--- app/src/main/java/com/akexorcist/deviceinformation/module/feature/holder/FeatureUnknownViewHolder.java
+++ app/src/main/java/com/akexorcist/deviceinformation/module/feature/holder/FeatureUnknownViewHolder.java
@@ -2,19 +2,14 @@ package com.akexorcist.deviceinformation.module.feature.holder;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.TextView;
-
-import com.akexorcist.deviceinformation.R;
 
 /**
  * Created by Akexorcist on 2/17/2017 AD.
  */
 
-public class FeatureHeaderViewHolder extends RecyclerView.ViewHolder {
-    public TextView tvHeader;
+public class FeatureUnknownViewHolder extends RecyclerView.ViewHolder {
 
-    public FeatureHeaderViewHolder(View itemView) {
+    public FeatureUnknownViewHolder(View itemView) {
         super(itemView);
-        tvHeader = (TextView) itemView.findViewById(R.id.tv_feature_header);
     }
 }
