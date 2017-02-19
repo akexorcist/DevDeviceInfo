25446bc39e9ebe4bf711c6e3bf68f761c4342b6c
Akexorcist <akexorcist@gmail.com>
2017-02-19 11:01:27 +0700

    Add description text id name

--- app/src/main/java/com/akexorcist/deviceinformation/module/feature/holder/FeatureEmptyViewHolder.java
+++ app/src/main/java/com/akexorcist/deviceinformation/module/feature/holder/FeatureEmptyViewHolder.java
@@ -2,14 +2,19 @@ package com.akexorcist.deviceinformation.module.feature.holder;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.TextView;
+
+import com.akexorcist.deviceinformation.R;
 
 /**
  * Created by Akexorcist on 2/17/2017 AD.
  */
 
 public class FeatureEmptyViewHolder extends RecyclerView.ViewHolder {
+    public TextView tvEmptyDescription;
 
     public FeatureEmptyViewHolder(View itemView) {
         super(itemView);
+        tvEmptyDescription = (TextView) itemView.findViewById(R.id.tv_feature_empty_description);
     }
 }
