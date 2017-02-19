117eb688f4b0608e11fcb32ca26626a6fd2c58e5
Akexorcist <akexorcist@gmail.com>
2017-02-19 08:25:31 +0700

    Create empty feature view holder

---
+++ app/src/main/java/com/akexorcist/deviceinformation/module/feature/holder/FeatureEmptyViewHolder.java
@@ -0,0 +1,15 @@
+package com.akexorcist.deviceinformation.module.feature.holder;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by Akexorcist on 2/17/2017 AD.
+ */
+
+public class FeatureEmptyViewHolder extends RecyclerView.ViewHolder {
+
+    public FeatureEmptyViewHolder(View itemView) {
+        super(itemView);
+    }
+}
