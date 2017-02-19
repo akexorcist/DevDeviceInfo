3f0c9a842a9aaf9974bafa3bae72fc67684e402e
Akexorcist <akexorcist@gmail.com>
2017-02-19 10:00:50 +0700

    Add feature item parameter

--- app/src/main/java/com/akexorcist/deviceinformation/collector/feature/model/FeatureItem.java
+++ app/src/main/java/com/akexorcist/deviceinformation/collector/feature/model/FeatureItem.java
@@ -7,7 +7,38 @@ import com.akexorcist.deviceinformation.common.BaseInfo;
  */
 
 public class FeatureItem extends BaseInfo {
+    private static final String NAME = "Name";
+    private static final String PACKAGE_NAME = "Package Name";
+    private static final String MINIMUM_SDK = "Minimum SDK";
+
     public FeatureItem() {
         super();
+    }
+
+    public String getName() {
+        return getValueByTitle(NAME);
+    }
+
+    public FeatureItem setName(String name) {
+        setDataInfo(NAME, name);
+        return this;
+    }
+
+    public String getPackageName() {
+        return getValueByTitle(PACKAGE_NAME);
+    }
+
+    public FeatureItem setPackageName(String packageName) {
+        setDataInfo(PACKAGE_NAME, packageName);
+        return this;
+    }
+
+    public String getMinimumSdk() {
+        return getValueByTitle(MINIMUM_SDK);
+    }
+
+    public FeatureItem setMinimumSdk(String minimumSdk) {
+        setDataInfo(MINIMUM_SDK, minimumSdk);
+        return this;
     }
 }
