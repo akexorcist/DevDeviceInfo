eaa00baee9e5c9a88ea3a17cb195f8202fba2dd0
Akexorcist <akexorcist@gmail.com>
2017-02-21 06:36:25 +0700

    Add event shortener for too many multiple override method event listener

---
+++ app/src/main/java/com/akexorcist/deviceinformation/utility/EventShortener.java
@@ -0,0 +1,29 @@
+package com.akexorcist.deviceinformation.utility;
+
+import android.opengl.GLSurfaceView;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ * Created by Akexorcist on 2/21/2017 AD.
+ */
+
+public class EventShortener {
+    public static class GlSurfaceRenderer implements GLSurfaceView.Renderer {
+        @Override
+        public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+
+        }
+
+        @Override
+        public void onSurfaceChanged(GL10 gl, int width, int height) {
+
+        }
+
+        @Override
+        public void onDrawFrame(GL10 gl) {
+
+        }
+    }
+}
