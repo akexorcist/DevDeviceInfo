6ffdb96a8d7605efbce9668d9f2adba23d8d6439
Akexorcist <akexorcist@gmail.com>
2017-03-04 21:55:25 +0700

    Refactor scroller linear layout manager

--- app/src/main/java/com/akexorcist/deviceinformation/widget/ScrollerLinearLayoutManager.java
+++ app/src/main/java/com/akexorcist/deviceinformation/widget/ScrollerLinearLayoutManager.java
@@ -1,8 +1,44 @@
 package com.akexorcist.deviceinformation.widget;
 
+import android.content.Context;
+import android.graphics.PointF;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.LinearSmoothScroller;
+import android.support.v7.widget.RecyclerView;
+
 /**
  * Created by Akexorcist on 3/4/2017 AD.
  */
 
-public class ScrollerLinearLayoutManager {
+public class ScrollerLinearLayoutManager extends LinearLayoutManager {
+    public ScrollerLinearLayoutManager(Context context) {
+        super(context, VERTICAL, false);
+    }
+
+    public ScrollerLinearLayoutManager(Context context, int orientation, boolean reverseLayout) {
+        super(context, orientation, reverseLayout);
+    }
+
+    @Override
+    public void smoothScrollToPosition(RecyclerView recyclerView, RecyclerView.State state, int position) {
+        RecyclerView.SmoothScroller smoothScroller = new TopSnappedSmoothScroller(recyclerView.getContext());
+        smoothScroller.setTargetPosition(position);
+        startSmoothScroll(smoothScroller);
+    }
+
+    private class TopSnappedSmoothScroller extends LinearSmoothScroller {
+        public TopSnappedSmoothScroller(Context context) {
+            super(context);
+        }
+
+        @Override
+        public PointF computeScrollVectorForPosition(int targetPosition) {
+            return ScrollerLinearLayoutManager.this.computeScrollVectorForPosition(targetPosition);
+        }
+
+        @Override
+        protected int getVerticalSnapPreference() {
+            return SNAP_TO_START;
+        }
+    }
 }
