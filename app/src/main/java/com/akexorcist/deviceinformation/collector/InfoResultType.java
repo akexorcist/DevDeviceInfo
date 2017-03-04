fee90a4479bf0802c259b9b03ee6d940fec279ce
Akexorcist <akexorcist@gmail.com>
2017-03-04 21:16:26 +0700

    Create info result type

---
+++ app/src/main/java/com/akexorcist/deviceinformation/collector/InfoResultType.java
@@ -0,0 +1,11 @@
+package com.akexorcist.deviceinformation.collector;
+
+/**
+ * Created by Akexorcist on 3/4/2017 AD.
+ */
+
+public class ResultType {
+    public static final String TYPE_YES = "Yes";
+    public static final String TYPE_NO = "No";
+    public static final String TYPE_UNKNOWN = "Unknown";
+}
