e54898350551490dbeb470d1ce3954cb55d6b3a6
Akexorcist <akexorcist@gmail.com>
2017-03-04 18:29:45 +0700

    Remove extending base info class

--- app/src/main/java/com/akexorcist/deviceinformation/collector/feature/model/FeatureInfo.java
+++ app/src/main/java/com/akexorcist/deviceinformation/collector/feature/model/FeatureInfo.java
@@ -1,14 +1,12 @@
 package com.akexorcist.deviceinformation.collector.feature.model;
 
-import com.akexorcist.deviceinformation.common.BaseInfo;
-
 import java.util.List;
 
 /**
  * Created by Akexorcist on 11/22/2016 AD.
  */
 
-public class FeatureInfo extends BaseInfo {
+public class FeatureInfo {
     private List<FeatureItem> supportedFeatureItem;
     private List<FeatureItem> unsupportedFeatureItem;
 
