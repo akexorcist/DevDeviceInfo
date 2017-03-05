e6a465276eabfae0364f026ecd12cc09aefce8b9
Akexorcist <akexorcist@gmail.com>
2017-03-06 01:28:40 +0700

    Add upload device activity

---
+++ app/src/main/java/com/akexorcist/deviceinformation/module/upload/UploadDeviceActivity.java
@@ -0,0 +1,80 @@
+package com.akexorcist.deviceinformation.module.upload;
+
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+
+import com.akexorcist.deviceinformation.R;
+import com.akexorcist.deviceinformation.common.BaseDdiActivity;
+import com.akexorcist.deviceinformation.utility.EventShortener;
+import com.hanks.htextview.HTextView;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+public class UploadDeviceActivity extends BaseDdiActivity {
+    private GLSurfaceView svOpenGl;
+    private HTextView tvStatus;
+
+    @Override
+    protected int getLayoutView() {
+        return R.layout.activity_upload_device;
+    }
+
+    @Override
+    protected void bindView() {
+        svOpenGl = (GLSurfaceView) findViewById(R.id.sv_upload_device_open_gl);
+        tvStatus = (HTextView) findViewById(R.id.tv_upload_device_status);
+    }
+
+    @Override
+    protected void setupView() {
+        svOpenGl.setBackgroundColor(ContextCompat.getColor(this, R.color.extra_extra_light_gray));
+        svOpenGl.setEGLContextClientVersion(2);
+        svOpenGl.setRenderer(onSetRenderer());
+        tvStatus.animateText("Collect Data...");
+    }
+
+    @Override
+    protected void setupToolbar() {
+
+    }
+
+    @Override
+    protected void restoreArgument(Bundle bundle) {
+
+    }
+
+    @Override
+    protected void initialize() {
+
+    }
+
+    @Override
+    public void restoreInstanceState(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    public void restoreView() {
+
+    }
+
+    @Override
+    public void saveInstanceState(Bundle outState) {
+
+    }
+
+    private EventShortener.GlSurfaceRenderer onSetRenderer() {
+        return new EventShortener.GlSurfaceRenderer() {
+            @Override
+            public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+                collectAllInfo(gl);
+            }
+        };
+    }
+
+    private void collectAllInfo(GL10 gl10) {
+        // TODO Collect all info
+    }
+}
