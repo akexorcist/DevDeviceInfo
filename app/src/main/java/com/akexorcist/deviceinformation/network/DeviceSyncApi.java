285481796f883986c05233ffbd8973731529d67f
Akexorcist <akexorcist@gmail.com>
2017-03-14 04:49:46 +0700

    Create service api

---
+++ app/src/main/java/com/akexorcist/deviceinformation/network/DeviceSyncApi.java
@@ -0,0 +1,27 @@
+package com.akexorcist.deviceinformation.network;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by Akexorcist on 3/14/2017 AD.
+ */
+
+public class DeviceSyncApi {
+    private static DeviceSyncApi api;
+
+    public static DeviceSyncApi getInstance() {
+        if (api == null) {
+            api = new DeviceSyncApi();
+        }
+        return api;
+    }
+
+    public DeviceSyncService getApi() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(DeviceSyncUrl.BASE)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        return retrofit.create(DeviceSyncService.class);
+    }
+}
