9f8c9df375368885b418c0d5576275eca4366d76
Akexorcist <akexorcist@gmail.com>
2017-03-14 04:43:58 +0700

    Add service url

---
+++ app/src/main/java/com/akexorcist/deviceinformation/network/DeviceSyncUrl.java
@@ -0,0 +1,16 @@
+package com.akexorcist.deviceinformation.network;
+
+/**
+ * Created by Akexorcist on 3/14/2017 AD.
+ */
+
+public class DeviceSyncUrl {
+    public static final String BASE = "";
+    public static final String ADD_DEVICE = "/api/v1/ddi/device";
+    public static final String GET_ALL_DEVICE = "/api/v1/ddi/devices";
+    public static final String GET_DEVICE_INFO = "/api/v1/ddi/device";
+    public static final String GET_ALL_BRAND = "/api/v1/ddi/getAllBrand";
+    public static final String GET_MODEL_BY_BRAND = "/api/v1/ddi/getModelByBrand";
+    public static final String GET_VERSION_BY_INFO = "/api/v1/ddi/getVersionByInfo";
+    public static final String SEARCH_BY_KEYWORD = "/api/v1/ddi/search";
+}
