653085dfa55329b5e194a0fcf4f986bce2c1cf03
Akexorcist <akexorcist@gmail.com>
2017-04-13 21:07:32 +0700

    Add custom application class

---
+++ app/src/main/java/com/akexorcist/deviceinformation/module/DdiApplication.java
@@ -0,0 +1,14 @@
+package com.akexorcist.deviceinformation.module;
+
+import android.app.Application;
+
+/**
+ * Created by Akexorcist on 3/30/2017 AD.
+ */
+
+public class DdiApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
